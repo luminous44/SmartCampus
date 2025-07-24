4a1be3aeceb9242908a7fd4577226c7fdfe60a61
luminous44 <aklemasultanakhi@gmail.com>
2025-07-24 21:03:32 +0600

    added student view and controller api

--- src/main/java/com/smartcampus/campus/Controller/StudentController.java
+++ src/main/java/com/smartcampus/campus/Controller/StudentController.java
@@ -3,10 +3,17 @@ package com.smartcampus.campus.Controller;
 import com.smartcampus.campus.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
     @Autowired
     private StudentService service;
 
+    @GetMapping("/students")
+    public String getAllStudents(Model model){
+        model.addAttribute("students",service.getAllStudent());
+        return "student";
+    }
 }
